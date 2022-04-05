b5323cdd988475c7f40302997f4c41cd1572599d
allan muzeya <allan.thecodemaster@gmail.com>
2022-04-05 23:02:26 +0200

    user details migration

---
+++ database/migrations/2022_04_05_205031_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('user_details', function(Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->string('coverImage')->nullable();
+            $table->string('profilePicture');
+            $table->string('bio')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
