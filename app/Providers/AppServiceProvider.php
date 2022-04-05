9a70c9cacd23c6478ed8c6e730f3c1156657e22e
Batsirai-sudo <batsybbay@gmail.com>
2022-04-05 17:16:28 +0200

    Registration styling and backend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Http\Services\StudentLevels\StudentLevels;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $viewShare['studentLevels'] = (new StudentLevels())->getAll();
+        view()->share($viewShare);
     }
 }
