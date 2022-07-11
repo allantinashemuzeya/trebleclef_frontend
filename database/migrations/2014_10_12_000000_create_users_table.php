4abb9bbc43e6d7ef8bb92b52e9981ec89e90b37d
Allan Muzeya <allan.muzeya@wundermanthompson.com>
2022-07-11 12:54:12 +0200

    ad default to name field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
-            $table->string('name');
+            $table->string('name')->default('');
             $table->string('lastname');
             $table->unsignedBigInteger('userType')->default(1);
             $table->string('email')->unique();
