84da874d9b011e8d2e542556e3c6f7423052e314
Allan Muzeya <allan.muzeya@wundermanthompson.com>
2022-07-11 13:01:48 +0200

    search by firstname

--- app/Http/Controllers/vendor/Chatify/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/MessagesController.php
@@ -383,7 +383,7 @@ class MessagesController extends Controller
         $getRecords = null;
         $input = trim(filter_var($request['input']));
         $records = User::where('id','!=',Auth::user()->id)
-                    ->where('name', 'LIKE', "%{$input}%")
+                    ->where('firstname', 'LIKE', "%{$input}%")
                     ->paginate($request->per_page ?? $this->perPage);
         ray($records)->red();
         foreach ($records->items() as $record) {
