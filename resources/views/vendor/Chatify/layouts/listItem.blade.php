0b55f010caf2a2cf040f00e3f35300bde2919fb3
Allan Muzeya <allan.muzeya@wundermanthompson.com>
2022-07-17 22:21:50 +0200

    listitem stuff

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -30,56 +30,55 @@
         style="background-image: url('{{ $user->avatar }}');">
         </div>
         </td>
-        {{-- center side --}}
-        <td>
-        <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
-            <span>{{ $lastMessage->created_at->diffForHumans() }}</span></p>
-        <span>
-            {{-- Last Message user indicator --}}
-            {!!
-                $lastMessage->from_id == Auth::user()->id
-                ? '<span class="lastMessageIndicator">You :</span>'
-                : ''
-            !!}
-            {{-- Last message body --}}
-            @if($lastMessage->attachment == null)
-            {!!
-                strlen($lastMessage->body) > 30
-                ? trim(substr($lastMessage->body, 0, 30)).'..'
-                : $lastMessage->body
-            !!}
-            @else
-            <span class="fas fa-file"></span> Attachment
-            @endif
-        </span>
-        {{-- New messages counter --}}
-            {!! $unseenCounter > 0 ? "<b>".$unseenCounter."</b>" : '' !!}
-        </td>
 
-    </tr>
-</table>
-@endif
-
-{{-- -------------------- Search Item -------------------- --}}
-@if($get == 'search_item')
-<table class="messenger-list-item" data-contact="{{ $user->id }}">
-    <tr data-action="0">
-        {{-- Avatar side --}}
-        <td>
-        <div class="avatar av-m"
-        style="background-image: url('{{ $user->avatar }}');">
-        </div>
-        </td>
         {{-- center side --}}
         <td>
             <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+                {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+                <span>{{ $lastMessage->created_at->diffForHumans() }}</span></p>
+            <span>
+                {{-- Last Message user indicator --}}
+                {!!
+                    $lastMessage->from_id == Auth::user()->id
+                    ? '<span class="lastMessageIndicator">You :</span>'
+                    : ''
+                !!}
+                {{-- Last message body --}}
+                @if($lastMessage->attachment == null)
+                {!!
+                    strlen($lastMessage->body) > 30
+                    ? trim(substr($lastMessage->body, 0, 30)).'..'
+                    : $lastMessage->body
+                !!}
+                @else
+                <span class="fas fa-file"></span> Attachment
+                @endif
+            </span>
+            {{-- New messages counter --}}
+                {!! $unseenCounter > 0 ? "<b>".$unseenCounter."</b>" : '' !!}
         </td>
-
     </tr>
 </table>
 @endif
+
+{{----------------------- Search Item -------------------- --}}
+    @if($get == 'search_item')
+        <table class="messenger-list-item" data-contact="{{ $user->id }}">
+            <tr data-action="0">
+
+                {{-- Avatar side --}}
+                <td>
+                    <div class="avatar av-m" style="background-image: url('{{ $user->avatar }}');"></div>
+                </td>
+
+                {{-- center side --}}
+                <td>
+                    <p data-id="{{ $user->id }}" data-type="user">
+                    {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+                </td>
+            </tr>
+        </table>
+    @endif
 
 {{-- -------------------- Shared photos Item -------------------- --}}
 @if($get == 'sharedPhoto')
