c4106b8f201a9c61bcae619522dd13afdaa0b222
Allan Muzeya <allan.muzeya@wundermanthompson.com>
2023-01-25 19:32:54 +0100

    Fix profile pic error

--- app/Http/Services/Students/Student.php
+++ app/Http/Services/Students/Student.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Services\Students;
 
-use GuzzleHttp\Client;
+use App\Models\User;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\MultipartStream;
 use Illuminate\Support\Facades\Storage;
 use JetBrains\PhpStorm\NoReturn;
-use App\Models\User;
 
 
 class Student implements StudentsInterface
@@ -32,59 +29,62 @@ class Student implements StudentsInterface
     {
         // TODO: Implement createStudent() method.
 
-        $client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => env('CMS_BASE_URL'),
-            // You can set any number of default request options.
-            'timeout'  => 2.0,
-        ]);
-
         $studentData = $user->student()->first();
 
-        $path = Storage::disk('public')->path('/profilePictures/'.$studentData->profile_picture);
+        $path = Storage::disk('public')->path('/profilePictures/' . $studentData->profile_picture);
 
-        $multipart = [
-            [
-                'name'     => 'data',
-                'contents' => json_encode([
-                    'type' => 'node--student',
-                    'attributes' => [
-                        'langcode' => 'en',
-                        'status' => true,
-                        'title' => $user->name,
-                        'field_cellphone_number' => $studentData->cellphoneNumber,
-                        'field_date_of_birth' => $studentData->date_of_birth,
-                        'field_first_name' => $studentData->name,
-                        'field_gender' => $studentData->gender,
-                        'field_last_name' => $studentData->last_name,
-                        'field_user_id' => $studentData->user_id,
-                    ],
-                    'relationships' => [
-                        'field_profile_picture' => [
-                            'data' => [
-                                'type' => 'file--file',
-                                'alt' => 'Profile Picture',
-                                'title' => 'Cover Image',
-                            ]
-                        ]
-                    ],
-                ]),
-            ],
+        $curl = curl_init();
 
-            [
-                'name'     => 'files[field_profile_picture]',
-                'contents' => fopen($path, 'r'),
-                'filename' => $studentData->profile_picture,
-            ],
-        ];
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'cms.trebleclefapp.co.za/jsonapi/node/student',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+    "data": {
+        "type": "node--student",
+        "attributes": {
+            "langcode": "en",
+            "status": true,
+            "title": "Allan T Muzeya",
+            "field_cellphone_number": "748454087",
+            "field_date_of_birth": "2023-01-23",
+            "field_first_name": "Allan",
+            "field_gender": "male",
+            "field_last_name": "Muzeya",
+            "field_next_of_kin_contact": "748454087",
+            "field_next_of_kin_full_name": "Martha Makro",
+            "field_postal_address": "1322",
+            "field_residential_address": "1322 Cameroon Street",
+            "field_student_bio": "i love you so much",
+            "field_user_id": 1,
+            "field_network_profile_picture": null
+        },
+        "relationships": {
+            "field_school": {
+                "data": {
+                    "type": "node--schools",
+                    "id": "1b1b6045-a9d2-4339-b561-4249d727098e"
+                }
+            }
+        }
+    }
+}',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/vnd.api+json',
+                'Authorization: Basic YWxsYW46S3VuZ2Z1Y29vbDI0'
+            ),
+        ));
 
-        $response = $client->request('POST', env('BACKEND_API').'student', [
-            'headers' => [
-                'Content-Type' => 'multipart/form-data',
-            ],
-            'multipart' => $multipart,
-            'auth' => [env('CMS_WRITE_USERNAME'), env('CMS_WRITE_PASSWORD')],
-        ]);
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        dd($response);
+
 
     }
 
