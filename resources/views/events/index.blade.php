eadb265149ca77c77aba6a95c0d048f16131f0e3
amuzeya <allan.muzeya@ceo-vision.com>
2023-02-01 22:51:34 +0100

    quickFixBugsAndImprovements

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -22,7 +22,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
-    <title>Dashboard ecommerce - Vuexy - Bootstrap HTML admin template</title>
+    <title>Trebleclef Academy | Events</title>
     <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.html">
     <link rel="shortcut icon" type="image/x-icon" href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
